10ee1feb8f920ee49edc38429ed41151d738f8fa
Pablo <jdommay863@g.educaand.es>
2024-04-26 18:44:28 +0200

    sessiones

--- tu-proyecto/src/main/java/com/miproyecto/servlets/MiServlet.java
+++ tu-proyecto/src/main/java/com/miproyecto/servlets/MiServlet.java
@@ -1,12 +1,15 @@
 package com.miproyecto.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequestAttributeEvent;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Servlet de ejemplo
@@ -32,7 +35,24 @@ public class MiServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	      response.getWriter().print("Hola desde MiServlet!");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("test session attributess <br>");
+		HttpSession sesion = request.getSession();
+		
+		System.out.println("<-- "  ); 
+		synchronized (sesion) {
+			sesion.setAttribute("foo", "22");
+			sesion.setAttribute("foo", "42");
+			
+			out.println(sesion.getAttribute("foo"));
+		}
+		/*
+		request.setAttribute("clave", "valor");
+		request.removeAttribute("clave");
+	      response.getWriter().print("Hola desde MiServlet!");*/
 	}
 
 	/**
@@ -41,6 +61,10 @@ public class MiServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	public void attributeTest(ServletRequestAttributeEvent ev) {
+		System.out.println("---> " + ev.getName() + "-->" + ev.getValue() ); 
 	}
 
 }
